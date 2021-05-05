2786bc9ef84046d5de8b5b1fb058d9735b647308
toehmler <roehmler@middlebury.edu>
2021-05-05 14:52:21 -0700

    test commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div id="app">
-    <!--
     <div id="nav" class="text-gray-700 dark:text-white">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    -->
     <router-view/>
   </div>
 </template>
