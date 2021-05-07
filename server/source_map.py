0d37ca7f3cbf9d4783c203deaa2951f81b99b8a8
toehmler <roehmler@middlebury.edu>
2021-05-06 17:47:56 -0700

    switched to real api call (not seed data) to test

--- server/source_map.py
+++ server/source_map.py
@@ -68,6 +68,7 @@ class source_map:
 
     def fetch_articles(self, bias, source_list):
 #        source_list = self.fetch_sources(bias)
+        '''
         if bias < 0:
             articles = self.left_articles
         else:
@@ -90,12 +91,12 @@ class source_map:
             articles.extend(hits)
 
         articles.sort(key = lambda x: arrow.get(x['pubDate']), reverse=True)
-
+        '''
 
         out_fname = f'{bias}_results.json'
         out_file = open(out_fname, 'w')
         json.dump(articles, out_file)
         out_file.close()
+        '''
         return (source_list, articles)
         #return (source_list, self.articles)
-        '''
